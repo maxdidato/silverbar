b68ee61f7cef3b0d61a1cadfb626b938fa44dc4c
mdidato <d>
2017-08-29 17:28:07 +0100

    feat(): adds a test for keyvaluestorage.getAllValues

--- src/test/java/com/maxdidato/silverbar/datalayer/KeyValueStorageTest.java
+++ src/test/java/com/maxdidato/silverbar/datalayer/KeyValueStorageTest.java
@@ -3,6 +3,10 @@ package com.maxdidato.silverbar.datalayer;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -21,6 +25,19 @@ public class KeyValueStorageTest {
         int key = 1;
         keyValueStorage.addValue(key, objectToBeInserted);
         assertThat(keyValueStorage.retrieveValue(key), is(objectToBeInserted));
+
+    }
+
+    @Test
+    public void it_returns_all_the_entries() {
+        keyValueStorage.addValue(1, "Object1");
+        keyValueStorage.addValue(2, "Object2");
+        keyValueStorage.addValue(3, "Object3");
+
+        LinkedHashMap allValues = keyValueStorage.getAllValues();
+        assertThat(allValues.values().size(), is(3));
+        assertThat(new ArrayList<>(allValues.values()), is(Arrays.asList("Object1", "Object2", "Object3")));
+
 
     }
 }
