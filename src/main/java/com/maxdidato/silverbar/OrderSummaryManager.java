e578cab0aedb0c296aeb6fbd9bd3327bf725c5cd
mdidato <d>
2017-09-02 17:46:22 +0100

    feat(): orderSummaryManager aggregates SELL orders by price

--- src/main/java/com/maxdidato/silverbar/OrderSummaryManager.java
+++ src/main/java/com/maxdidato/silverbar/OrderSummaryManager.java
@@ -2,14 +2,34 @@ package com.maxdidato.silverbar;
 
 import com.maxdidato.silverbar.domain.Order;
 import com.maxdidato.silverbar.domain.OrderSummaryRow;
+import com.maxdidato.silverbar.domain.OrderType;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+import static com.maxdidato.silverbar.domain.OrderType.SELL;
 
 public class OrderSummaryManager {
 
     public List<OrderSummaryRow> generate(List<Order> orders) {
-        return null;
+        //using a treemap using the order price as key to apply the natural order
+        TreeMap summary = new TreeMap<BigDecimal,OrderSummaryRow>();
+        List<Order> filteredOrders = orders.stream().filter(o -> o.getOrderType().equals(SELL)).collect(Collectors.toList());
+        filteredOrders.forEach(o -> {
+            OrderSummaryRow orderSummaryRow = new OrderSummaryRow(o.getPricePerKilos(),o.getKilos());
+            Object previousValue = summary.put(orderSummaryRow.getPrice(), orderSummaryRow);
+            if (previousValue != null) {
+                double aggregatedWeight = ((OrderSummaryRow)previousValue).getKilos() + o.getKilos();
+                orderSummaryRow.setKilos(aggregatedWeight);
+                summary.put(orderSummaryRow.getPrice(),orderSummaryRow);
+            }
+        });
 
+        return new LinkedList<>(summary.values());
 
     }
 }
--- src/test/java/com/maxdidato/silverbar/manager/OrderSummaryManagerTest.java
+++ src/test/java/com/maxdidato/silverbar/manager/OrderSummaryManagerTest.java
@@ -1,8 +1,51 @@
 package com.maxdidato.silverbar.manager;
 
+import com.maxdidato.silverbar.LiveOrderBoard;
+import com.maxdidato.silverbar.OrderSummaryManager;
+import com.maxdidato.silverbar.domain.Order;
+import com.maxdidato.silverbar.domain.OrderSummaryRow;
+import com.maxdidato.silverbar.domain.OrderType;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import static com.maxdidato.silverbar.domain.OrderType.BUY;
+import static com.maxdidato.silverbar.domain.OrderType.SELL;
 import static org.junit.jupiter.api.Assertions.*;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
-class OrderSummaryManagerTest {
+public class OrderSummaryManagerTest {
+
+    private OrderSummaryManager orderSummaryManager;
+
+    @Before
+    public void setUp() {
+        orderSummaryManager = new OrderSummaryManager();
+    }
+
+    @Test
+    public void it_aggregates_orders_with_sell_type_by_price() {
+        List<Order> orders = new ArrayList<>();
+        orders.add(new Order().withKilos(2)
+                .withPricePerKilos(new BigDecimal(200)).withOrderType(SELL));
+        orders.add(new Order().withKilos(3)
+                .withPricePerKilos(new BigDecimal(200)).withOrderType(SELL));
+        orders.add(new Order().withKilos(2)
+                .withPricePerKilos(new BigDecimal(300)).withOrderType(SELL));
+        orders.add(new Order().withKilos(5)
+                .withPricePerKilos(new BigDecimal(300)).withOrderType(SELL));
+
+        List<OrderSummaryRow> orderSummaryRows = new LinkedList<>();
+        orderSummaryRows.add(new OrderSummaryRow().withKilos(5).withPrice(new BigDecimal(200)));
+        orderSummaryRows.add(new OrderSummaryRow().withKilos(7).withPrice(new BigDecimal(300)));
+        assertThat(orderSummaryManager.generate(orders),is(orderSummaryRows));
+    }
+
 
 }
